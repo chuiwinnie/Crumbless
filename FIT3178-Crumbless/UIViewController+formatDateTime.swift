1d933492e896f5242b821605dfa23e1784f8e235
Winnie Chui <cchu0061@student.monash.edu>
2023-06-07 07:19:04 +1000

    Add formatTime function

---
+++ FIT3178-Crumbless/UIViewController+formatDateTime.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController+formatDateTime.swift
+//  FIT3178-Crumbless
+//
+//  Created by Winnie Chui on 26/4/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func formatDate(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        return dateFormatter.string(from: date)
+    }
+    
+    func formatTime(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm a"
+        return dateFormatter.string(from: date)
+    }
+}
+
+
+/**
+ References:
+ - Formatting time: https://stackoverflow.com/questions/61546387/how-to-get-only-time-from-date-in-swift
+ */
